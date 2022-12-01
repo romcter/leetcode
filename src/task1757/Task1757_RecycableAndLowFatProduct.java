5f14fc13004b4b493b43ca7ce42214c5cdeaea84
RomanPryimuk <roman.pryimuk@resmed.com>
2022-12-02 01:25:12 +0200

    Task 1758: recyclable and low fat product

---
+++ src/task1757/Task1757_RecycableAndLowFatProduct.java
@@ -0,0 +1,6 @@
+package task1757;
+
+public class Task1757_RecycableAndLowFatProduct {
+
+    String solution = "select p.product_id from Products p where p.low_fats = 'Y' and p.recyclable = 'Y';";
+}
